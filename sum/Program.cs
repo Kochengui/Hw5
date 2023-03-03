cf494cbb3cc99d1a36fae73ed1fcb0bd3bae77cc
Anton <nachalnikkochengui@gmail.com>
2023-03-03 22:35:16 +0800

    sm

---
+++ sum/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] nums = new int[size];
+FillArray(nums);
+Console.WriteLine("Вот наш массив: ");
+PrintArray(nums);
+int sum = 0;
+
+for (int i = 0; i < nums.Length; i+=2)
+    sum = sum + nums[i];
+
+    Console.WriteLine($"всего {nums.Length} чисел/числа, сумма элементов на нечётных позициях = {sum}");
+
+void FillArray(int[] nums)
+{
+    for(int i = 0; i < nums.Length; i++)
+        {
+            nums[i] = new Random().Next(1,10);
+        }
+}
+void PrintArray(int[] nums)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < nums.Length; i++)
+        {
+            Console.Write(nums[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
